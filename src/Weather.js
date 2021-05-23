cd9fd260c1acfcdafbbb34c6aad38a400d5597c1
kierab <78439567+kierabon@users.noreply.github.com>
2021-05-22 22:27:59 -0500

    Adjusted css added search box

--- src/Weather.js
+++ src/Weather.js
@@ -4,20 +4,31 @@ import "./Weather.css";
 export default function Weather(){
     return(
         <div className="Weather">
+            <form className="form-inline">
+            <div className="row">
+                <div className="col-9">
+            
+                <input type="search" placeholder="Enter a city" autoComplete="off"className="form-control" /> 
+                </div>
+                <div className="col">
+                <input type="submit" value="Search" className="btn btn-primary search" />
+           
+            </div> </div>  </form>
+            <br />
             <h1>
             New York, NY
             </h1>
-            <ul>
+            <ul id="list">
                 <li> Mostly Cloudy</li>
                 <li>Saturday 7:00</li>
             </ul>
             <div className="row">
-            <div className="col-6"> 
-            
-            76F
+            <div className="col-6" id="temp"> 
+            🌫
+            76℉
             </div>
             <div className="col-6">
-                <ul>
+                <ul id="details">
                     <li> Percipitation= 70%</li>
                     <li> wind </li>
                     <li> humidity</li>
